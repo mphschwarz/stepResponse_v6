e56e006e60f5902b870827eb2f945b1b44564920
mphschwarz@gmail.com <mphschwarz@gmail.com>
2017-04-26 15:48:53 +0200

    added subfunctions

---
+++ matlab/stepI.m
@@ -0,0 +1,12 @@
+% s: step index
+
+% x: data input vector
+% t: time input vector
+% n: filter order
+% start: user defined approx. start
+function s = findI(x,t,n,start)
+
+[xxmax, sxmax] = findpeaks(x(start:end))	%first peak after start
+
+
+end
