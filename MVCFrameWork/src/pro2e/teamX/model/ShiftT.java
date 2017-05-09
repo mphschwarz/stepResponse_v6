b8d997878763b6961751649107c0c545da4dd8e8
KennethSteiner <kenneth.steiner@students.fhnw.ch>
2017-05-09 12:42:18 +0200

    ComboBox

---
+++ MVCFrameWork/src/pro2e/teamX/model/ShiftT.java
@@ -0,0 +1,24 @@
+package pro2e.teamX.model;
+
+import pro2e.teamX.matlabfunctions.Matlab;
+
+public class ShiftT {
+
+	public double[] shiftT(double[] t, double[] y, double[] c, int n, int N) {		
+		int ix5 = 1;
+		int ixm = 1;
+		int ix0 = 1;
+		
+		int iy5 = 1;
+		int iym = 1;
+		int iy0 = 1;
+		
+		tx = Matlab.linspace(0, t.length-1, t.length);
+		
+		
+		while (x)
+		
+		return di;
+	}	
+	
+}
