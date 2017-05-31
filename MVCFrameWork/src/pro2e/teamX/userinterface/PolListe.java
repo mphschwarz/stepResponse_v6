4726a1a72b8e0a498d600272b98a07ba1bdd9ec9
KennethSteiner <kenneth.steiner@students.fhnw.ch>
2017-05-31 14:57:20 +0200

    Update: PolLIste

--- MVCFrameWork/src/pro2e/teamX/userinterface/PolListe.java
+++ MVCFrameWork/src/pro2e/teamX/userinterface/PolListe.java
@@ -32,64 +32,72 @@ import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import org.jfree.util.ResourceBundleSupport;
+
 import pro2e.teamX.MVCFramework;
+import pro2e.teamX.matlabfunctions.Matlab;
 import pro2e.teamX.model.Model;
 
 public class PolListe extends JPanel {
-     public JTextArea jtpols = new JTextArea(5, 20);
-     private double[] xWert = new double[10];
-     private double[] yWert = new double[10];
+	public JTextArea jtpols = new JTextArea(5, 20);
+	private double[] xWert = new double[21];
+	private double[] yWert = new double[21];
+	private String pole = "";
 
-     public void main(double[] reBestOrd, double[] imBestOrd) {
-           for (int i = 0; i < reBestOrd.length; i++) {
-                reBestOrd[i] = Math.round(1000.0 * reBestOrd[i]) / 1000.0;
-                xWert[i] = reBestOrd[i];
-                System.out.println(" Real " + reBestOrd[i]);
-           }
-           for (int i = 0; i < imBestOrd.length; i++) {
-                imBestOrd[i] = Math.round(1000.0 * imBestOrd[i]) / 1000.0;
-                yWert[i] = imBestOrd[i];
-                System.out.println(" Imag " + imBestOrd[i]);
-           }
-           StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < xWert.length; i++) {
-            if (xWert[i]!=0 || yWert[i]!=0)
-            {
-                 
-            if (yWert[i]<0)
-              sb.append(" ").append(xWert[i]).append(" ").append(yWert[i]).append(" j\n");
-            else
-                 sb.append(" ").append(xWert[i]).append(" +").append(yWert[i]).append(" j\n");
-        }
-        }
-           Test(sb.toString());
-     }
+	public void main(double[] reBestOrd, double[] imBestOrd) {
+		for (int i = 0; i < reBestOrd.length; i++) {
+			reBestOrd[i] = Math.round(100.0 * reBestOrd[i]) / 100.0;
+			xWert[i] = reBestOrd[i];
+			System.out.println(" Real " + reBestOrd[i]);
+		}
+		xWert = Matlab.colon(xWert, 0, reBestOrd.length-1);
+		for (int i = 0; i < imBestOrd.length; i++) {
+			imBestOrd[i] = Math.round(100.0 * imBestOrd[i]) / 100.0;
+			yWert[i] = imBestOrd[i];
+			System.out.println(" Imag " + imBestOrd[i]);
+		}
+		yWert = Matlab.colon(yWert, 0, imBestOrd.length-1);
+		pole = "";
+		for (int i = 0; i < xWert.length; i++) {
+			pole += "P"+(i+1)+":\t" + xWert[i] + " + " + yWert[i] + "i\n";
+		}
+		Test();
+	}
 
-     public PolListe() {
-           this.setLayout(new GridLayout(1, 1));
-           this.setPreferredSize(new Dimension(250, 300)); 
-     }
-     
-     public void Test(String string) {
-           jtpols.setBackground(getBackground());
-           jtpols.setEditable(false);
-           jtpols.setFont(getFont().deriveFont(14.0f));
-           jtpols.setSize(400, 400);
-           jtpols.setText(string);
-           add(jtpols, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                     new Insets(10, 10, 10, 10), 0, 0));
-     }
+	public PolListe() {
+		this.setLayout(new GridLayout(1, 1));
+		this.setPreferredSize(new Dimension(250, 300));
+		//Test();
+	}
 
-     public void update(Observable obs, Object obj) {
-           Model model = (Model) obs;
-           System.out.println("Test Pol Liste");
-           //                              if(model.gerechnet==false)
-           //                                            main(model.reBestOrd, model.imBestOrd);
-           if (model.gerechnet == true) {
-                main(model.reBestOrd, model.imBestOrd);
-           for (int i = 0; i < model.reBestOrd.length; i++) {
-                System.out.println(model.reBestOrd[i]);
-           }}
-     }
+	public void Test() {
+		jtpols.setBackground(getBackground());
+		jtpols.setEditable(false);
+		jtpols.setFont(getFont().deriveFont(14.0f));
+		jtpols.setSize(400, 400);
+		/*jtpols.setText(" " + xWert[0] + " + " + yWert[0] + " i\n" + " " + xWert[1] + " + " + yWert[1] + " i\n" + " " + xWert[2]
+				+ " + " + yWert[2] + " i\n" + " " + xWert[3] + " + " + yWert[3] + " i\n" + " " + xWert[4] + " + " + yWert[4]
+				+ " i\n" + " " + xWert[5] + " + " + yWert[5] + " i\n" + " " + xWert[6] + " + " + yWert[6] + " i\n" + " "
+				+ xWert[7] + " + " + yWert[7] + " i\n" + " " + xWert[8] + " + " + yWert[8] + " i\n" + " " + xWert[9] + " + "
+				+ yWert[9] + " i\n");*/
+		jtpols.setText(pole);
+		add(jtpols, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(10, 10, 10, 10), 0, 0));
+		System.out.println("!!!!!");
+
+	}
+
+	public void update(Observable obs, Object obj) {
+		Model model = (Model) obs;
+		System.out.println("Test Pol Liste");
+		//                              if(model.gerechnet==false)
+		//                                            main(model.reBestOrd, model.imBestOrd);
+		if (model.gerechnet == true) {
+			//model.gerechnet = false;
+			main(model.reBestOrd, model.imBestOrd);
+			for (int i = 0; i < model.reBestOrd.length; i++) {
+				System.out.println(model.reBestOrd[i]);
+			}
+		}
+	}
 }
-
