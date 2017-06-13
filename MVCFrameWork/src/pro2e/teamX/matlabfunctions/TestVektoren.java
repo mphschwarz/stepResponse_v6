9e86474237427a25a3d0aa8c1f02c3eb7a418443
KennethSteiner <kenneth.steiner@students.fhnw.ch>
2017-06-13 10:26:41 +0200

    Clear all old MVC

--- MVCFrameWork/src/pro2e/teamX/matlabfunctions/TestVektoren.java
+++
@@ -1,7 +0,0 @@
-package pro2e.teamX.matlabfunctions;
-
-public class TestVektoren {
-
-	double [] A = {  1, 	     2,	     3};
-	
-}
--- MVCFrameWork/src/pro2e/teamX/model/Filter.java
+++
@@ -1,5 +0,0 @@
-package pro2e.teamX.model;
-
-public class Filter extends Model {
-
-}
